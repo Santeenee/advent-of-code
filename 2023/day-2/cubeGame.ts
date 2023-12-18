8b0a83ef30e436e096b36305321cef2ef361fead
Francesco Santini <francescotn02@gmail.com>
2023-12-18 01:27:29 +0100

    DAY 2: part 1 complete!

--- 2023/day-2/cubeGame.ts
+++ 2023/day-2/cubeGame.ts
@@ -1,9 +1,9 @@
 import fs from 'fs'
 import splitNewLine from '../../utils/splitNewLine'
 
-// ----------------
-const isDemo = true
-// ----------------
+// -----------------
+const isDemo = false
+// -----------------
 
 let input: string[] = []
 try {
@@ -14,26 +14,77 @@ try {
 	console.error('error reading input file\n', error)
 }
 
-//types
-type Round = {
-	reds: number
-	greens: number
-	blues: number
-}
-
-type Game = Round[]
-
-type Games = Game[]
-
 //constants
 const BAG = {
-	reds: 12,
-	greens: 13,
-	blues: 14,
+	red: 12,
+	green: 13,
+	blue: 14,
 } as const
 
-const games: {}[] = []
+const START_CHAR = ':'
+const ROUND_SEPARATOR = ';'
+const BALL_SEPARATOR = ','
+
+const gameIds: number[] = []
 
 console.log(BAG)
 
 // function getCubesPerGame(bag) {}
+
+const matrix = input.map(game => game.split(''))
+
+matrix.forEach((game, gameIndex) => {
+	let validGame = true
+	const startGame = game.indexOf(START_CHAR) + 2
+
+	const roundsEndIndexes: number[] = []
+	const roundsSlice: string[][] = []
+
+	//find all ';'
+	game.forEach((char, nChar) => {
+		if (char === ROUND_SEPARATOR) roundsEndIndexes.push(nChar)
+	})
+
+	//create slice for each round
+	for (let round = 0; round < roundsEndIndexes.length + 1; round++) {
+		roundsSlice[round] = game.slice(
+			round === 0 ? startGame : roundsEndIndexes[round - 1] + 2,
+			roundsEndIndexes[round] ?? undefined
+		)
+	}
+
+	roundsSlice.forEach(round => {
+		const ballsEndIndexes: number[] = []
+		const ballSlice: string[][] = []
+
+		//find all ':'
+		round.forEach((char, nChar) => {
+			if (char === BALL_SEPARATOR) ballsEndIndexes.push(nChar)
+		})
+
+		//create slice for each ball
+		for (let ball = 0; ball < ballsEndIndexes.length + 1; ball++) {
+			ballSlice[ball] = round.slice(
+				ball === 0 ? 0 : ballsEndIndexes[ball - 1] + 2,
+				ballsEndIndexes[ball] ?? undefined
+			)
+			//TODO say if this game is "POSSIBLE with <n> amount of <color> balls"
+			const [num, color] = ballSlice[ball].join('').split(' ')
+
+			Object.entries(BAG).forEach(([bagColor, bagNum]) => {
+				if (bagColor === color && +num > bagNum) {
+					validGame = false
+				}
+			})
+		}
+		// console.log(ballSlice.map(ball => ball.join('')))
+	})
+	if (validGame) gameIds.push(gameIndex + 1)
+})
+
+const result = [...new Set(gameIds)].reduce((a, b) => a + b)
+
+console.log(gameIds)
+console.log(result)
+// console.log(matrix)
+// console.log(startGame)
