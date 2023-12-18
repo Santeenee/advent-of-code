cf858b13148f21e6e2b0166260a6d4642e203a2c
Francesco Santini <francescotn02@gmail.com>
2023-12-18 17:34:34 +0100

    createDay.sh adds first input read in index.ts

--- 2023/day-3/index.ts
+++ 2023/day-3/index.ts
@@ -1,1 +1,17 @@
 // TODO solve the AoC challenge of the day
+import fs from 'fs'
+import splitNewLine from '../../utils/splitNewLine'
+
+// -----------------
+const isDemo = true
+// -----------------
+
+let input: string[] = []
+try {
+	isDemo
+		? (input = splitNewLine(fs.readFileSync('demoInput.txt').toString()))
+		: (input = splitNewLine(fs.readFileSync('input.txt').toString()))
+} catch (error) {
+	console.error('error reading input file\n', error)
+}
+
--- createDay.sh
+++ createDay.sh
@@ -44,7 +44,23 @@ npm pkg delete 'scripts.test'
 
 # create necessary files
 gitignore node VisualStudio
-echo '// TODO solve the AoC challenge of the day' > index.ts
+echo '// TODO solve the AoC challenge of the day
+import fs from '\''fs'\''
+import splitNewLine from '\''../../utils/splitNewLine'\''
+
+// -----------------
+const isDemo = true
+// -----------------
+
+let input: string[] = []
+try {
+	isDemo
+		? (input = splitNewLine(fs.readFileSync('\''demoInput.txt'\'').toString()))
+		: (input = splitNewLine(fs.readFileSync('\''input.txt'\'').toString()))
+} catch (error) {
+	console.error('\''error reading input file\n'\'', error)
+}
+' > index.ts
 echo '# Thinking' > THINKING.md
 touch input.txt
 touch demoInput.txt
