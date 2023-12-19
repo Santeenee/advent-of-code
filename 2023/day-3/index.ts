4f5ff99e9ed07bfd7627ee98a549ca48b708077f
Francesco Santini <francescotn02@gmail.com>
2023-12-19 22:19:06 +0100

    DAY 3: part 2 idk what im doing

--- 2023/day-3/index.ts
+++ 2023/day-3/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import splitNewLine from '../../utils/splitNewLine'
 
 // -----------------
-const isDemo = false
+const isDemo = true
 // -----------------
 
 let input: string[] = []
@@ -27,9 +27,16 @@ type Symbol = {
 	col: number
 }
 
+type Coordinates = {
+	char: string
+	row: number
+	col: number
+}
+
 // * DATA
 const PARTS: Part[] = []
 const SYMBOLS: Symbol[] = []
+const GEARS: Coordinates[] = []
 
 const numbersRegexp = /\d+/g
 const symbolRegexp = /(?!\.)\W/g
@@ -68,44 +75,82 @@ input.forEach((line, nLine) => {
 for (let nPart = 0; nPart < PARTS.length; nPart++) {
 	const part = PARTS[nPart]
 
-	const { row, cols } = part
-	let isEnginePart = false
+	const {
+		num,
+		row,
+		cols: { startCol },
+	} = part
 
-	//check all surroundings
-	//check above
-	//check left and right
-	//check below
-	//if none of these 4, then splice(x, 1)
-
-	//check left
-	if (onlySymbolRegexp.test(input[row][cols.startCol - 1])) {
-		isEnginePart = true
+	const coord = {
+		char: num.toString(),
+		row: row,
+		col: startCol,
 	}
-	//check right
-	if (onlySymbolRegexp.test(input[row][cols.endCol + 1])) {
-		isEnginePart = true
-	}
-	//check above and below
-	for (let nChar = cols.startCol - 1; nChar <= cols.endCol + 1; nChar++) {
-		let charAbove = '.'
-		let charBelow = '.'
-
-		if (input[row - 1]) charAbove = input[row - 1][nChar]
-		if (input[row + 1]) charBelow = input[row + 1][nChar]
-
-		if (onlySymbolRegexp.test(charAbove)) isEnginePart = true
-		if (onlySymbolRegexp.test(charBelow)) isEnginePart = true
-	}
+	const enginePart = searchAroundAString(coord, onlySymbolRegexp)
 
 	//DELETE part because it's not an engine part
-	if (!isEnginePart) {
+	if (!enginePart) {
 		console.log('Non engine part', part)
 		PARTS.splice(nPart, 1)
 		nPart--
 	}
 }
 
-const RESULT = PARTS.map(part => part.num).reduce((a, b) => a + b)
+//find engine gears
+for (let nSymbol = 0; nSymbol < SYMBOLS.length; nSymbol++) {
+	const symbol = SYMBOLS[nSymbol]
+
+	const foundCoord = searchAroundAString(symbol, numbersRegexp, 2)
+	if (Array.isArray(foundCoord)) GEARS.push(...foundCoord)
+}
+
+function searchAroundAString(
+	coord: Coordinates,
+	regexp: RegExp,
+	howManyMinimum = 1
+) {
+	const { char, row, col } = coord
+	let foundCoord: Coordinates[] = []
+	const endCol = col + char.length - 1
+
+	//check left
+	if (regexp.test(input[row][col - 1])) {
+		foundCoord.push({ char: input[row][col - 1], row, col: col - 1 })
+	}
+	//check right
+	if (regexp.test(input[row][endCol + 1])) {
+		foundCoord.push({ char: input[row][endCol + 1], row, col: endCol + 1 })
+	}
+	//check above and below
+	for (let nChar = col - 1; nChar <= endCol + 1; nChar++) {
+		let charAbove = '.'
+		let charBelow = '.'
+
+		if (input[row - 1]) charAbove = input[row - 1][nChar]
+		if (input[row + 1]) charBelow = input[row + 1][nChar]
+
+		if (regexp.test(charAbove))
+			foundCoord.push({ char: charAbove, row: row - 1, col: nChar })
+
+		if (regexp.test(charBelow))
+			foundCoord.push({ char: charBelow, row: row + 1, col: nChar })
+	}
+
+	//! findNumberSGivenOneCoordinateOMGHOW()
+
+	if (foundCoord.length >= howManyMinimum) {
+		if (howManyMinimum === 1) {
+			return foundCoord[0]
+		}
+		return foundCoord
+	}
+	return
+}
+
+const ENGINE_PARTS_SUM = PARTS.map(part => part.num).reduce((a, b) => a + b) // part 1
+const GEAR_RATIO_SUM = GEARS.filter(item => item !== undefined) //! try remove .filter()
+	.map(item => Number(item.char))
+	.reduce((a, b) => a + b) // part 2
 
 console.log('\nINPUT')
 console.table(isDemo ? input : input.slice(0, 20))
@@ -116,6 +161,7 @@ console.table(isDemo ? PARTS : PARTS.slice(0, 20))
 console.log('\nSYMBOLS')
 console.table(isDemo ? SYMBOLS : SYMBOLS.slice(0, 20))
 
-console.log('\nAll engine parts combined make this number:', RESULT)
+console.log('\nAll engine parts combined make this number:', ENGINE_PARTS_SUM)
+console.log('\nAll gears combined make this gear ratio:', GEAR_RATIO_SUM)
 
 //564199 too high
